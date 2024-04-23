9926bc8fea7359fced998790817c242e219dc556
Michealmiker <michealmiker@gmail.com>
2024-04-23 22:24:14 +0800

    Upload SequentialList

---
+++ cs/Test/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using DataStructure.Linear.List;
---
+++ cs/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿#define SEQUENTIAL_LIST
+
+#if SEQUENTIAL_LIST
+var seqList = new SequentialList<int>();
+
+for (var i = 0; i < 10; i++)
+{
+    seqList.Add(i);
+}
+
+Console.WriteLine(seqList);
+Console.WriteLine();
+
+seqList.Remove();
+seqList.RemoveFirst();
+seqList.AddFirst(100);
+
+foreach (var elem in seqList)
+{
+    Console.WriteLine(elem);
+}
+#endif
